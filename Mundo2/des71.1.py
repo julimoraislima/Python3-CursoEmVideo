1f5fc05cf505dc328e84ba157a33eea3af7de86e
Juliana de Morais Lima <limamju@hotmail.com>
2021-01-18 09:47:59 +0100

    Create des71.1.py

---
+++ Mundo2/des71.1.py
@@ -0,0 +1,9 @@
+#desafio 71: Simulador de caixa eletrônico. USANDO O FOR
+
+valorSaque = int(input('Valor do saque: R$ '))
+print('-' * 40)
+for nota in [50, 20, 10, 1]:
+    quantidade = valorSaque // nota
+    valorSaque = valorSaque % nota
+    if quantidade > 0:
+        print(f'{quantidade} notas de R$ {nota}')
